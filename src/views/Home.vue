83708155c366293637bb6ef30986639630e20c70
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-01-26 22:48:22 +0600

    Template Refs in the Setup Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,35 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   name: "Home",
   setup() {
     console.log("setup");
+//Refs
+    const p = ref(null) //getting reference of <p>
+                        //cant use before return{} the value
+    console.log(p, p.value) //it will be 'null'
+                        //because it is not returned{} yet
+
     let name = "Mario";
     let age = 30;
-//we can perform any JavaScript coding into setup() function
-//we can create function, variable, object, import function
-//from other component by importing them etc..
-//we can perform Option API as well as Composition API simulteneously
-//Option API's generally think to for simple project
-//Composition API think to complex and large project
-//But both are useful under the circumstances
 
 const handleClick = () => {
-  console.log('you clicked me')
+  console.log(p, p.value)
 }
 
-//we can return{} the values inside the setup() function
-//if we want to make them accessible in the "<template>"
-
-//This is the very basic of how to use the Compositon API
-//We make a setup function or setup if you prefer to call it that way
-//That alwasy runs first before created before mounted etc.
-//And inside this setup(), we can run any JavaScript
-//Such as create values or create functions
-//Then at the end down(line 38) here in return{}, we just return whatever values
-//or functions we want to make available to the component <template>.
     return {
       name,
       age,
-      handleClick
+      handleClick,
+      p
     };
   },
 };
