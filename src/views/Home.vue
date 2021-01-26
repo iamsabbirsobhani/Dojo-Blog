38120f46bc7e4ed1d98af21efc4dddf154634841
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-01-26 23:34:03 +0600

    Using Refs for Reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,13 @@
   <div class="home">
     home
     <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <!-- ref is for accessing ref() from setup -->
     <button @click="handleClick">Click me</button>
+    <button @click="age++" >Add 1 to age</button>
+    <!-- we can access directly setup() returned values with the click event
+    or any event -->
+    <input type="text" v-model="name">
+    <!-- for accesing values from setup() don't need to add value after
+      properties like name.value or age.value etc-->
   </div>
 </template>
 
@@ -13,29 +18,28 @@ export default {
   name: "Home",
   setup() {
     console.log("setup");
-//Refs
-    const p = ref(null) //getting reference of <p>
-                        //cant use before return{} the value
-    console.log(p, p.value) //it will be 'null'
-                        //because it is not returned{} yet
+//Using Refs for Reactivity
+//By default setup() values are not Reactive
+//Using ref() we can make values reactive like
+//Options API function data(){return{}}
 
-    let name = "Mario";
-    let age = 30;
+    const name = ref("Mario") //v-model="name" is updating the "ref(value)" value
+                              //not the "name". "name" is updating according to the
+                              //return values of "ref()" into the "name" variable
+                              //In "setup()", "this" token is "undefined"
+    const age = ref(30)
 
 const handleClick = () => {
-  console.log(p, p.value)
-  p.value.classList.add("test")
-  p.value.textContent = 'Hello, ninjas'
-  //we can perform regular JS using ref()
-  //but not before return{} performed
-  //handleClick is performing after click happened and return{} happended as well
+  name.value = 'luigi'
+  age.value = 35
 }
 
     return {
       name,
       age,
-      handleClick,
-      p
+      handleClick
+    // in order to access all of the values of setup() function
+    //all the values/properties should be returned
     };
   },
 };
