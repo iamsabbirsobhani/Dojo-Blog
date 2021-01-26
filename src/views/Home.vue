58b33c852b7a488a97767bf082c456511e2a1c36
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-01-26 21:51:18 +0600

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
     home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Home',
+  name: "Home",
+  setup() {
+    console.log("setup");
+    let name = "Mario";
+    let age = 30;
 
-}
+    return {
+      name,
+      age
+    };
+  },
+};
 </script>
