0aa0876b8a5ee50e37cad32df129d7f461b78c6f
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-01-27 13:08:40 +0600

    Refs vs Reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,59 @@
 <template>
   <div class="home">
-    home
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++" >Add 1 to age</button>
-    <!-- we can access directly setup() returned values with the click event
-    or any event -->
-    <input type="text" v-model="name">
-    <!-- for accesing values from setup() don't need to add value after
-      properties like name.value or age.value etc-->
+    <h1>Home</h1>
+      <h2>Refs</h2>
+      <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+      <button @click="updateNinjaOne">Update ninja</button>
+
+      <h2>Reactive</h2>
+      <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
+      <button @click="updateNinjaTwo">Update ninja</button>
+      <!-- ninjaTwo,ninjaOne is an object and name, age its property or key
+      so, for accessing "name" key we should declare ninjaTwo.name or ninjaOne.name
+      generally we do in JavaScript-->
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 export default {
   name: "Home",
   setup() {
-    console.log("setup");
-//Using Refs for Reactivity
-//By default setup() values are not Reactive
-//Using ref() we can make values reactive like
-//Options API function data(){return{}}
+   const ninjaOne = ref({ name: 'mario', age: 30})
+   const ninjaTwo = reactive({ name: 'luigi', age: 35 })
 
-    const name = ref("Mario") //v-model="name" is updating the "ref(value)" value
-                              //not the "name". "name" is updating according to the
-                              //return values of "ref()" into the "name" variable
-                              //In "setup()", "this" token is "undefined"
-    const age = ref(30)
+                                     //Note: ref() vs reactive()
+   const nameOne = ref('mario')      //(a)we can use any values(primitive or reference type)
+                                     //with ref()
+   const nameTwo = reactive('luigi') //(b)we can't use "primitive values" with reactive
+                                     //or "reactive()" loses its reactive power when it gets "primitive data type".
+                                     //here 'luigi' is a String primitive date type
+                                     //so we can't update this value or this
+                                     //(c)reactive(primitive type) !== reacitve value
+                                     //(d)reactive(reference type) === reactive value
+                                     //One last feature is,
+                                     //(d)we don't have to add ".value" extention to use reactive() data
+                                     //example: for ref() we should use "ninjaOne.value.age"
+                                     //(e)but for "reactive()" we can access value without ".value" extention
+                                     //example: ninjaTwo.name, ninjaTwo.age
+                                     //Otherwise both are Reactive values
+   const updateNinjaOne = () => {
+     ninjaOne.value.age = 40
+   }
 
-const handleClick = () => {
-  name.value = 'luigi'
-  age.value = 35
-}
+   const updateNinjaTwo = () => {
+     ninjaTwo.age = 45
+     nameTwo = 'mario'  //doesn't update value in the "<template>"
+     nameTwo = reactive('mario') //it also doesn't update value in the "<template>" as well
+
+   }
 
     return {
-      name,
-      age,
-      handleClick
-    // in order to access all of the values of setup() function
-    //all the values/properties should be returned
+      ninjaOne,
+      updateNinjaOne,
+      ninjaTwo,
+      updateNinjaTwo,
+      nameTwo
     };
   },
 };
