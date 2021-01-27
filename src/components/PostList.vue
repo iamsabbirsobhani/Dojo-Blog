5e76cef659098102fb7e9cf86cd3c0c8ca28996a
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-01-27 22:23:54 +0600

    Composition API Lifecycle Hooks

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,13 +7,20 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
 import SinglePost from './SinglePost.vue'
 export default {
     components: { SinglePost },
     props: ['posts'],
     setup(props){
-        console.log(props.posts)
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component unmounted'))
+        onUpdated(() => console.log('component updated'))
+    },
+    mounted() {
+        console.log('mounted using mounted()')
     }
+
 }
 </script>
 
