a309ffdb854aa43bf00333cbdaeee89f66a9a052
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-02-05 13:42:47 +0600

    added delete option

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,6 +3,7 @@
     <div v-if="post" class="post">
         <h3>{{ post.title }}</h3>
         <p class="pre">{{ post.body }}</p>
+        <button class="btn3" @click="handleDelete">delete post</button>
     </div>
     <div v-else>
         <Spin/>
@@ -12,6 +13,8 @@
 <script>
 import getPost from '../composables/getPost'
 import Spin from '../components/Spinner'
+import { projectFirestore } from '../firebase/config'
+import { useRouter } from 'vue-router'
 
 export default {
     props: ['id'],
@@ -19,11 +22,20 @@ export default {
     setup(props){
         const { post, error,  load} = getPost(props.id)
 
+        const router = useRouter()
+
         load()
+
+        const handleDelete = async () => {
+            await projectFirestore.collection('posts').doc(props.id).delete()
+
+            router.push({ name: 'Home' })
+        }
 
         return{
             post,
-            error
+            error,
+            handleDelete
         }
     }
 }
@@ -41,5 +53,10 @@ export default {
     }
     .pre{
         white-space: pre-wrap;
+    }
+    .btn3{
+        margin: 10px auto;
+        cursor: pointer;
+        background-color: red;
     }
 </style>
