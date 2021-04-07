c767e4e13dace879403a6130f297edbd3afdc87e
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-04-07 09:29:08 +0600

    added new firebaseConfig API key  on production mode

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBs_1LBupexsMAVy4lyQupX19l259Ap9_Y",
-  authDomain: "dojo-bl.firebaseapp.com",
-  projectId: "dojo-bl",
-  storageBucket: "dojo-bl.appspot.com",
-  messagingSenderId: "636777854264",
-  appId: "1:636777854264:web:f059b60257693cf002bdcd"
+  apiKey: "AIzaSyABdetQ3ecG9sl29JUZhumGfNbFKewrRQ0",
+  authDomain: "dojo-blog-9e6b5.firebaseapp.com",
+  projectId: "dojo-blog-9e6b5",
+  storageBucket: "dojo-blog-9e6b5.appspot.com",
+  messagingSenderId: "1026740091951",
+  appId: "1:1026740091951:web:19b64428dcbc02fb751ada",
+  measurementId: "G-ET3T5E7L91"
 };
 
 // init firebase
